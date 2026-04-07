01a73c67708857ba985a13e6b425eca7ed677be5
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-07 11:04:56 -0700

    Step 2 complete

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
 let score = 0;
 let pointsPerClick = 1;
+let clickBtn = document.getElementById("click-btn");
 
 function updateDisplay() {
-    document.getElementById("score-display").textContent = 'Score: ' + score;
-    document.getElementById("rate-display").textContent = 'Points per click: ' + pointsPerClick;
+  document.getElementById("score-display").textContent = "Score: " + score;
+  document.getElementById("rate-display").textContent =
+    "Points Per Click: " + pointsPerClick;
 }
 
+clickBtn.addEventListener("click", function () {
+  score += pointsPerClick;
+  updateDisplay();
+});
