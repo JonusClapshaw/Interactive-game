de2b27fa954d73c68dd22577a95a0c3cf71f08de
Jonus <joclapshaw@gmail.com>
2026-04-07 11:11:38 -0700

    Array of objects and upgrades

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 let score = 0;
 let pointsPerClick = 1;
+let upgrades = [
+    { id: 1, Name: increase, Cost: 5, Upgrade: 1 },
+    { id: 2, Name: increase, Cost: 25, Upgrade: 10 },
+    { id: 3, Name: increase, Cost: 500, Upgrade: 100 }
+];
 let clickBtn = document.getElementById("click-btn");
 
 function updateDisplay() {
