55d01090e3b087cf81fb2dcb96426bbeab4f69c2
Jonus <joclapshaw@gmail.com>
2026-04-07 16:36:28 -0700

    Finished styling and UI

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let score = 0;
 let pointsPerClick = 1;
 let upgradeDiv = document.getElementById("upgrades");
+let coinDropArea = document.getElementById("coin-drop-area");
 let upgrades = [
   { id: 1, name: "increase1", Cost: 5, Upgrade: 1 },
   { id: 2, name: "increase2", Cost: 25, Upgrade: 10 },
@@ -9,15 +10,33 @@ let upgrades = [
 let clickBtn = document.getElementById("click-btn");
 
 function updateDisplay() {
-  document.getElementById("score-display").textContent = "Score: " + score;
+  document.getElementById("score-display").textContent = "Coins: " + score;
   document.getElementById("rate-display").textContent =
-    "Points Per Click: " + pointsPerClick;
+    "Coins Per Click: " + pointsPerClick;
+}
+
+function dropCoins() {
+  //Limit the number of coins dropped
+  const coinCount = Math.min(pointsPerClick, 8);
+
+  for (let index = 0; index < coinCount; index += 1) {
+
+    const coin = document.createElement("span");
+    // puts the coin on the screen and subtract 24 cause the size of the coin is 24px
+    coin.className = "coin";
+    coin.style.left = Math.random() * (coinDropArea.clientWidth - 24) + "px";
+
+    // coin drop area is the div where coins drop and we will remove coin from screen after animation ends
+    coinDropArea.appendChild(coin);
+    coin.addEventListener("animationend", () => coin.remove());
+  }
 }
 
 clickBtn.addEventListener("click", function () {
   score += pointsPerClick;
   updateDisplay();
   renderUpgrades();
+  dropCoins();
 });
 
 function renderUpgrades() {
@@ -48,8 +67,8 @@ updateDisplay();
 renderUpgrades();
 
 function buyUpgrade(id) {
-  const upgrade = upgrades.find(u => u.id === id);
-  
+  const upgrade = upgrades.find((u) => u.id === id);
+
   if (score >= upgrade.Cost) {
     score -= upgrade.Cost;
     pointsPerClick += upgrade.Upgrade;
